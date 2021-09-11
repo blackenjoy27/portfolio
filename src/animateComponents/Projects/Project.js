382db2776d720b1e5632faeb0e51ffab4ff7f903
blackenjoy27 <lixunxin@gmail.com>
2021-09-11 18:09:31 -0400

    wip

--- src/animateComponents/Projects/Project.js
+++ src/animateComponents/Projects/Project.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Project = ({ project }) => {
+const Project = ({ project, changeBG }) => {
     return (
         <div>
-            <div className={`${project.color} projectDiv`}>
+            <div className={`${project.color} projectDiv`}
+                onMouseOver={() => changeBG(project.bgClass)}
+                onMouseOut={() => changeBG("")}>
                 <h1 className="projectLink">
                     <a href={project.liveVersion}>
                         {project.projectName}
@@ -11,7 +13,7 @@ const Project = ({ project }) => {
                 </h1>
                 <p className="projectP">Description: {project.description}</p>
             </div>
-        </div>
+        </div >
 
     )
 }
--- src/animateComponents/Projects/Projects.js
+++ src/animateComponents/Projects/Projects.js
@@ -9,26 +9,37 @@ const allProjects = [
         color: "redOne",
         projectName: "Potluck Planner",
         liveVersion: "https://frontend-17dulzzug-blackenjoy27.vercel.app",
-        description: "Potluck Planner is an app that allows organizers and attendees to efficiently plan their potluck events."
+        description: "Potluck Planner is an app that allows organizers and attendees to efficiently plan their potluck events.",
+        backGroundUrl: "/tech-wallpaper-2.jpeg",
     },
     {
         color: "blueOne",
         projectName: "Family Promises",
         liveVersion: "https://a.familypromiseservicetracker.dev/login",
-        description: "Family Promises is an app that helps homeless families going through their hardship and prevent them from becoming homeless again."
+        description: "Family Promises is an app that helps homeless families going through their hardship and prevent them from becoming homeless again.",
+        backGroundUrl: "/tech-wallpaper-2.jpeg",
+    },
+    {
+        color: "greenOne",
+        projectName: "Use My Tech",
+        liveVersion: "https://a.familypromiseservicetracker.dev/login",
+        description: "Use My Tech is an app that provides the most effient way to get the tech from other people for a short time usage. All users can choose to be a borrower or a renter",
+        bgClass: "tech",
+
     },
 ]
 const Projects = () => {
-    const [projects, setProjects] = useState(allProjects);
-    const [index, setIndex] = useState(0);
+    const [projects] = useState(allProjects);
+    const [wallPaper, setWallPaper] = useState("");
+
     return (
-        <motion.div exit={{ transition: "easeInOut" }}>
+        <motion.div exit={{ transition: "easeInOut" }} className={`projects ${wallPaper}`}>
             <div className="top">
                 <motion.header
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     transition={{ duration: 1 }}
-                    className="Header"
+                    className="projectHeader"
                 >
                     <nav className="about-nav">
                         <Link to="/"><h4>Home</h4></Link>
@@ -38,7 +49,7 @@ const Projects = () => {
             </div>
             <div>
                 {projects && projects.map(project => {
-                    return <Project project={project} />
+                    return <Project project={project} changeBG={setWallPaper} />
                 })}
             </div>
         </motion.div>
